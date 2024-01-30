6de421d435d1d55b7cc8bfd461a9307991a84b3e
aliraza61199 <a.r06shorts@gmail.com>
2024-01-31 01:30:12 +0530

    1:30 Night

--- src/test/java/TestRunner/Test1.java
+++ src/test/java/TestRunner/Test1.java
@@ -7,6 +7,6 @@ import org.testng.annotations.Test;
 public class Test1 {
     @Test
     public void test01() {
-    	System.out.println("Hello Ali raza You Have Done " );
+    	System.out.println("Hello Ali raza You Have Done Email Notification" );
     }
 }
